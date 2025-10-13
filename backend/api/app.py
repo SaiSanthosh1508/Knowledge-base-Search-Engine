bc1b6dda03698b7c0764b559f46c9e56ec820c17
Thota Rishik Sai Santhosh <130997056+SaiSanthosh1508@users.noreply.github.com>
2025-10-14 00:43:39 +0530

    Update CORS allowed origins for API

--- backend/api/app.py
+++ backend/api/app.py
@@ -41,8 +41,8 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://knowledge-base-search-engine-theta.vercel.app/",
-        "https://localhost:5173"
+        "https://knowledge-base-search-engine-theta.vercel.app",
+        "http://localhost:5173"
     ],
     allow_credentials=True,
     allow_methods=["*"],
